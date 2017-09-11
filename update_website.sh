bd6e6d95d5fd4da12762f9d2e75bd54a045fd5ca
Sean Batzel <romulus108@protonmail.com>
2017-09-11 12:34:40 -0400

    Updated the sync script.

--- update_website.sh
+++ update_website.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git add *
-git commit
+git commit -m "$1"
 git push
 rm ../Web-Programming/* -rf
 cp * ../Web-Programming/
 cd ../Web-Programming/
 git add *
-git commit
+git commit -m "$1"
 git push
 echo "Done."
