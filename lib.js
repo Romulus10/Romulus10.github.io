77a1a1e243efec040c7a30589316adf3eb69ecf9
Sean Batzel <romulus108@protonmail.com>
2018-04-03 14:46:46 -0400

    Added another XKCD comic.

--- lib.js
+++ lib.js
@@ -1,5 +1,5 @@
 function random_comic() {
-    var comics = new Array('xkcd/7_eleven.png', 'xkcd/existence_proof.png', 'xkcd/hacking.png', 'xkcd/machine_learning.png', 'xkcd/who.png', 'xkcd/borrow_your_laptop.png', 'xkcd/existential_bug_reports.png', 'xkcd/here_to_help.png', 'xkcd/pointers.png', 'xkcd/color_pattern.png', 'xkcd/genetic_testing_results.png', 'xkcd/hottest_editors.png', 'xkcd/still_in_use.png');
+    var comics = new Array('xkcd/lisp.jpg', 'xkcd/7_eleven.png', 'xkcd/existence_proof.png', 'xkcd/hacking.png', 'xkcd/machine_learning.png', 'xkcd/who.png', 'xkcd/borrow_your_laptop.png', 'xkcd/existential_bug_reports.png', 'xkcd/here_to_help.png', 'xkcd/pointers.png', 'xkcd/color_pattern.png', 'xkcd/genetic_testing_results.png', 'xkcd/hottest_editors.png', 'xkcd/still_in_use.png');
     var rand_num = Math.floor(13*Math.random());
     var xkcd_comic = document.getElementById('xkcd_comic').setAttribute('src', comics[rand_num]);
 }
