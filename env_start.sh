34103f2423af8471f87737e84cce67e4bde865d2
Sean Batzel <romulus108@protonmail.com>
2022-04-25 19:44:10 -0400

    Restarting with React.

---
+++ env_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt update
+apt upgrade -y
+apt install -y nodejs
+apt autoremove -y
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
